8d4393b739862525a9edbe75cb38aaf50dd60965
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2025-11-21 16:44:55 +0100

    refactor: update the server to use express, cors and routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,14 @@
-import http from 'node:http'
+import express from 'express'
+import cors from 'cors'
+import routes from './routes'
 
-const server = http.createServer((req, res) => {
-  return res.end('hello world')
+const app = express()
+const PORT = 3333
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
 })
-
-server.listen(3333)
