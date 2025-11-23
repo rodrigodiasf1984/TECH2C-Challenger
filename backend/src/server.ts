4d10081c08e82ae69470106ad58b8677ab17f9df
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2025-11-23 18:07:54 +0100

    refactor: add .env for the backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,16 @@
-import express from 'express'
-import cors from 'cors'
-import routes from './routes'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
 
-const app = express()
-const PORT = 3333
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+const PORT = process.env.PORT || 3333;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
-})
+  console.log(`Server is running on ${PORT}`);
+});
